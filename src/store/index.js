ebfaccca973e296beea9198bd5af3c94a320790b
SeWoo-Kim <39786700+todnks@users.noreply.github.com>
2023-02-11 03:37:43 +0900

    firstcommit

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import listSlice from '../feature/list/listSlice';
-const store = configureStore({
-  reducer: { listSlice },
-});
-
-export default store;
