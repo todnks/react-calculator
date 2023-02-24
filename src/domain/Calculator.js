80614b88e248e4d18974b2473640a2205d480c23
SeWoo-Kim <39786700+todnks@users.noreply.github.com>
2023-02-24 11:21:30 +0900

    로직수정및 Infinity관련 오류문추가

--- src/domain/Calculator.js
+++ src/domain/Calculator.js
@@ -1,6 +1,8 @@
 import { Operator } from './index';
 
 export default class Calculator {
+  ERRORMSG = "오류";
+  static MAX_DIGITS_LENGTH = 3;
   constructor(operator, calculatorData) {
     this.operator = operator;
     this.calculatorData = calculatorData;
--- src/hooks/useCalculator.jsx
+++ src/hooks/useCalculator.jsx
@@ -1,47 +1,47 @@
-import { useMemo, useState } from 'react';
-import { useCallback } from 'react';
+import { useMemo, useState, useCallback } from 'react';
 import { Operator, Calculator } from '@/domain';
 const { symbols: SYMBOLS } = Operator;
+const { ERRORMSG } = Calculator;
 const intialState = '0';
 export default function useCalculator() {
+
   const [calculatorState, setState] = useState(intialState);
+
   const insertDigits = useCallback((digits) => {
-    if (calculatorState === intialState) {
+    if (calculatorState === intialState || calculatorState === ERRORMSG) {
       setState(digits);
       return;
     }
     setState(calculatorState + digits);
-    return;
   }, [calculatorState]);
 
   const insertOperation = useCallback((operator) => {
-    if (calculatorState === intialState) {
+    if (calculatorState === intialState || calculatorState === ERRORMSG) {
       return;
     }
     setState(calculatorState + operator);
-    return
   }, [calculatorState])
 
   const setAnswer = useCallback(() => {
     const operator = calculatorState.split('').find((item) => (
       SYMBOLS.includes(item)
     ))
-    const [a, b] = calculatorState.split(operator).map((item) => Number(item));
-    if (!operator || !a || !b) {
+    if (operator === undefined) {
       return;
     }
     const item = new Calculator(
       operator,
-      [a, b]
+      calculatorState.split(operator).map((item) => Number(item))
     );
     const result = item.execute();
+    if (result === Infinity) return setState(ERRORMSG);
     setState(String(result));
   })
-  const resetstate = () => setState(intialState);
+  const resetState = () => setState(intialState);
   return useMemo(() => ({
     insertDigits,
     calculatorState,
-    resetstate,
+    resetState,
     insertOperation,
     setAnswer
   }), [calculatorState])
