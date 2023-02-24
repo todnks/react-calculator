459608c9753b61ce29ad00a4d2c21f0040d2882b
SeWoo-Kim <39786700+todnks@users.noreply.github.com>
2023-02-24 11:22:21 +0900

    MAX_DIGITS_LENGTH 수정

--- src/domain/Validator.js
+++ src/domain/Validator.js
@@ -1,9 +1,11 @@
-import { Operator } from './index'
+import { Operator, Calculator } from './index'
 const { symbols: SYMBOLS, MAX_OPERATOR_LENGTH } = Operator;
-const MAX_DIGITS_LENGTH = 3;
+const { MAX_DIGITS_LENGTH } = Calculator;
+
 export default function Validator() {
 
   const isOverMaxDigitsLength = (text) => {
+
     const operator = text.split('').find((item) => (
       SYMBOLS.includes(item)
     ));
@@ -13,16 +15,21 @@ export default function Validator() {
         item.length <= MAX_DIGITS_LENGTH && item.length > 0
       ))
     };
+
     if (text.length <= MAX_DIGITS_LENGTH) return true;
+
   };
 
   const isOverMaxOperatorLength = (text) => {
+
     const operator = text.split('').filter((item) => (
       SYMBOLS.includes(item)
     ))
+
     if (operator.length <= MAX_OPERATOR_LENGTH) {
       return true;
     }
+
   }
   return {
     isOverMaxDigitsLength,
