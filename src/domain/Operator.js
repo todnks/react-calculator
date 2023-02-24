3d29549c2043e6b4dc338dcc080253079eb45858
SeWoo-Kim <39786700+todnks@users.noreply.github.com>
2023-02-24 11:20:29 +0900

    오류문추가 로직수정

--- src/domain/Operator.js
+++ src/domain/Operator.js
@@ -1,13 +1,13 @@
 export default class Operator {
   static MAX_OPERATOR_LENGTH = 1;
   static SYMBOLS = new Map([
-    ['+', new Operator('+', (a, b) => a + b)],
-    ['-', new Operator('-', (a, b) => a - b)],
-    ['X', new Operator('X', (a, b) => a * b)],
-    ['/', new Operator('/', (a, b) => Math.round(a / b))]]);
+    ['+', new Operator((a, b) => a + b)],
+    ['-', new Operator((a, b) => a - b)],
+    ['X', new Operator((a, b) => a * b)],
+    ['/', new Operator((a, b) => Math.floor(a / b))]
+  ]);
 
-  constructor(operator, calculator) {
-    this.operator = operator;
+  constructor(calculator) {
     this.calculator = calculator;
   }
 
@@ -16,7 +16,12 @@ export default class Operator {
   }
 
   static calculate(operator, [a, b]) {
-    const { calculator } = this.SYMBOLS.get(operator);
-    return calculator(a, b);
+    try {
+      const { calculator } = this.SYMBOLS.get(operator);
+      return calculator(a, b);
+    } catch (error) {
+      throw new Error(`${operator}정의 되지 않은 연산자입니다.`);
+    }
+
   }
 }
